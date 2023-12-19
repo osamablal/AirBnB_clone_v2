dc0c34575502949c2f5c474e54030a716e3be7ff
Osama Bilal <128569407+osamablal@users.noreply.github.com>
2023-12-19 14:19:10 +0200

    Add files via upload

---
+++ models/engine/file_storage.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""The class of File-Storage."""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+
+class FileStorage:
+    """
+        Putting instances to JSON and deputting to JSON.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self, cls=None):
+        """
+            Going back to the dictinary.
+        """
+        fsss_objects = {}
+        if cls:
+            if isinstance(cls) is str and cls in classes:
+                for key, val in self.__objects.items():
+                    if cls == key.split('.')[0]:
+                        fsss_objects[key] = val
+            elif cls.__name__ in classes:
+                for key, val in self.__objects.items():
+                    if cls.__name__ == key.split('.')[0]:
+                        fsss_objects[key] = val
+        else:
+            return self.__objects
+        return fsss_objects
+
+    def new(self, obj):
+        """
+            Putting the object with key <obj class name> id.
+        """
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        value_dict = obj
+        FileStorage.__objects[key] = value_dict
+
+    def save(self):
+        """
+            Putting objects attriebute to JSON.
+        """
+        objects_dict = {}
+        for key, val in FileStorage.__objects.items():
+            objects_dict[key] = val.to_dict()
+        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as fd:
+            json.dump(objects_dict, fd)
+
+    def reload(self):
+        """
+            Deputting the JSON file to objects.
+        """
+        try:
+            with open(FileStorage.__file_path, encoding="UTF8") as fd:
+                FileStorage.__objects = json.load(fd)
+            for key, val in FileStorage.__objects.items():
+                class_name = val["__class__"]
+                class_name = classes[class_name]
+                FileStorage.__objects[key] = class_name(**val)
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """
+        Deleting object from objects.
+        """
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + str(obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+        self.save()
+
+    def close(self):
+        """
+        Calling for reload.
+        """
+        self.reload()

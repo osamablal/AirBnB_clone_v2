fa475ae8bb8fbaeb5f6935c2389ce95d3ffb0b1c
el-hatem <elhaatem@gmail.com>
2023-12-21 03:26:15 +0200

    Ahmed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -82,7 +82,7 @@ class FileStorage:
                 FileStorage.__objects = json.load(fd)
             for key, val in FileStorage.__objects.items():
                 class_name = val["__class__"]
-                class_name = classes[class_name]
+                class_name = classes.get(class_name, class_name.split(".")[1])
                 FileStorage.__objects[key] = class_name(**val)
         except FileNotFoundError:
             pass

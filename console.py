922d23835213e883dbbebd364f49c942fa04ec76
el-hatem <elhaatem@gmail.com>
2023-12-21 04:39:40 +0200

    Ahmed

--- console.py
+++ console.py
@@ -49,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, args):
         """ A Method to exit HBNB console"""
-        print(type(args))
+        print(args)
         print("===========================")
         return True
 
